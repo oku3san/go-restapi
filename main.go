94113a2fd70022c62140fec0cb837ea187635388
oku3san <takaya0303@gmail.com>
2022-10-30 20:11:35 +0900

    p.42

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "io"
   "log"
   "net/http"
@@ -11,7 +12,34 @@ func main() {
     io.WriteString(w, "Hello, world!\n")
   }
 
-  http.HandleFunc("/", helloHandler)
+  postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "Posting Article...\n")
+  }
+
+  articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "Article List\n")
+  }
+
+  articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+    articleID := 1
+    resString := fmt.Sprintf("Article No.%d\n", articleID)
+    io.WriteString(w, resString)
+  }
+
+  postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "Posting Nice...\n")
+  }
+
+  postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "Posting Comment...\n")
+  }
+
+  http.HandleFunc("/hello", helloHandler)
+  http.HandleFunc("/article", postArticleHandler)
+  http.HandleFunc("/article/list", articleListHandler)
+  http.HandleFunc("/article1", articleDetailHandler)
+  http.HandleFunc("/article/nice", postNiceHandler)
+  http.HandleFunc("/comment", postCommentHandler)
 
   log.Println("server start at port 8080")
   err := http.ListenAndServe(":8080", nil)
