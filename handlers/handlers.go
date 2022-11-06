ad0c1130e21db0719dd5f759ad8a0ad067fe6bb8
oku3san <takaya0303@gmail.com>
2022-11-06 22:20:33 +0900

    p.114

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,11 +2,9 @@ package handlers
 
 import (
   "encoding/json"
-  "errors"
   "github.com/oku3san/go-restapi/models"
   "io"
   "net/http"
-  "strconv"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -14,32 +12,14 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-  length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-  if err != nil {
-    http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-    return
-  }
-  reqBodybuffer := make([]byte, length)
-
-  if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-    http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-    return
-  }
-  defer req.Body.Close()
-
   var reqArticle models.Article
-  if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+  if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
     http.Error(w, "fail to decode json\n", http.StatusBadRequest)
     return
   }
 
   article := reqArticle
-  jsonData, err := json.Marshal(article)
-  if err != nil {
-    http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-    return
-  }
-  w.Write(jsonData)
+  json.NewEncoder(w).Encode(article)
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
