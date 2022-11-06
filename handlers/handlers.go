a0c50cbec56a6a7eaa9eca8cc94031cf26d9b3de
oku3san <takaya0303@gmail.com>
2022-11-06 21:49:51 +0900

    p.95

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,12 +2,9 @@ package handlers
 
 import (
   "encoding/json"
-  "fmt"
-  "github.com/gorilla/mux"
   "github.com/oku3san/go-restapi/models"
   "io"
   "net/http"
-  "strconv"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -25,37 +22,63 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-  queryMap := req.URL.Query()
-
-  var page int
-  if p, ok := queryMap["page"]; ok && len(p) > 0 {
-    var err error
-    page, err = strconv.Atoi(p[0])
-    if err != nil {
-      http.Error(w, "Invalid query parameter", http.StatusBadRequest)
-      return
-    }
-  } else {
-    page = 1
+  //queryMap := req.URL.Query()
+  //
+  //var page int
+  //if p, ok := queryMap["page"]; ok && len(p) > 0 {
+  //  var err error
+  //  page, err = strconv.Atoi(p[0])
+  //  if err != nil {
+  //    http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+  //    return
+  //  }
+  //} else {
+  //  page = 1
+  //}
+  //resString := fmt.Sprintf("Article List (page %d)\n", page)
+  //io.WriteString(w, resString)
+  articleList := []models.Article{models.Article1, models.Article2}
+  jsonData, err := json.Marshal(articleList)
+  if err != nil {
+    http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
+    return
   }
-  resString := fmt.Sprintf("Article List (page %d)\n", page)
-  io.WriteString(w, resString)
+  w.Write(jsonData)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-  articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+  //articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+  //if err != nil {
+  //  http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+  //  return
+  //}
+  //resString := fmt.Sprintf("Article No.%d\n", articleID)
+  //io.WriteString(w, resString)
+  article := models.Article1
+  jsonData, err := json.Marshal(article)
   if err != nil {
-    http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+    http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
     return
   }
-  resString := fmt.Sprintf("Article No.%d\n", articleID)
-  io.WriteString(w, resString)
+  w.Write(jsonData)
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-  io.WriteString(w, "Posting Nice...\n")
+  article := models.Article1
+  jsonData, err := json.Marshal(article)
+  if err != nil {
+    http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
+    return
+  }
+  w.Write(jsonData)
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-  io.WriteString(w, "Posting Comment...\n")
+  comment := models.Comment1
+  jsonData, err := json.Marshal(comment)
+  if err != nil {
+    http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
+    return
+  }
+  w.Write(jsonData)
 }
