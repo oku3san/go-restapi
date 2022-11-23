2633b85cef3da1d3b2823044f96a4a0f7268e3a8
oku3san <takaya0303@gmail.com>
2022-11-23 21:36:49 +0900

    p.269

--- main.go
+++ main.go
@@ -5,32 +5,38 @@ import (
   "fmt"
   _ "github.com/go-sql-driver/mysql"
   "github.com/gorilla/mux"
-  "github.com/oku3san/go-restapi/handlers"
+  "github.com/oku3san/go-restapi/controllers"
+  "github.com/oku3san/go-restapi/services"
   "log"
   "net/http"
   "os"
 )
 
-func main() {
+var (
+  dbUser     = os.Getenv("DBUSERNAME")
+  dbPassword = os.Getenv("DBUSERPASS")
+  dbDatabase = os.Getenv("DATABASE")
+  dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+)
 
-  dbUser := os.Getenv("DBUSERNAME")
-  dbPassword := os.Getenv("DBUSERPASS")
-  dbDatabase := os.Getenv("DATABASE")
-  dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+func main() {
   db, err := sql.Open("mysql", dbConn)
   if err != nil {
-    fmt.Println(err)
+    fmt.Println("fail to connect DB")
   }
   defer db.Close()
-  
+
+  ser := services.NewMyAppService(db)
+  con := controllers.NewMyAppController(ser)
+
   r := mux.NewRouter()
 
-  r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-  r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-  r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+  r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
+  r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+  r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+  r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+  r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
+  r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
   log.Println("server start at port 8080")
   err2 := http.ListenAndServe(":8080", r)
