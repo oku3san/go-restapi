80c66d21fa2645a6f7ff0a716a656a45ae5aae1b
oku3san <takaya0303@gmail.com>
2022-11-24 22:03:09 +0900

    p.274

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
   "database/sql"
   "fmt"
   _ "github.com/go-sql-driver/mysql"
-  "github.com/gorilla/mux"
   "github.com/oku3san/go-restapi/controllers"
+  "github.com/oku3san/go-restapi/routers"
   "github.com/oku3san/go-restapi/services"
   "log"
   "net/http"
@@ -29,17 +29,8 @@ func main() {
 
   ser := services.NewMyAppService(db)
   con := controllers.NewMyAppController(ser)
-
-  r := mux.NewRouter()
-
-  r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-  r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-  r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+  r := routers.NewRouter(con)
 
   log.Println("server start at port 8080")
-  err2 := http.ListenAndServe(":8080", r)
-  log.Fatal(err2)
+  log.Fatal(http.ListenAndServe(":8080", r))
 }
