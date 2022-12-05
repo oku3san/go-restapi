8715ab613ca28410a14871e7968f1e1ac7ef1a95
oku3san <takaya0303@gmail.com>
2022-12-05 23:26:42 +0900

    p.398

--- api/router.go
+++ api/router.go
@@ -1,25 +1,28 @@
 package api
 
 import (
-  "database/sql"
-  "github.com/gorilla/mux"
-  "github.com/oku3san/go-restapi/controllers"
-  "github.com/oku3san/go-restapi/services"
-  "net/http"
+	"database/sql"
+	"github.com/gorilla/mux"
+	"github.com/oku3san/go-restapi/api/middlewares"
+	"github.com/oku3san/go-restapi/controllers"
+	"github.com/oku3san/go-restapi/services"
+	"net/http"
 )
 
 func NewRouter(db *sql.DB) *mux.Router {
-  ser := services.NewMyAppService(db)
-  aCon := controllers.NewArticleController(ser)
-  cCon := controllers.NewCommentController(ser)
-  r := mux.NewRouter()
+	ser := services.NewMyAppService(db)
+	aCon := controllers.NewArticleController(ser)
+	cCon := controllers.NewCommentController(ser)
+	r := mux.NewRouter()
 
-  r.HandleFunc("/hello", aCon.HelloHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article", aCon.PostArticleHandler).Methods(http.MethodPost)
-  r.HandleFunc("/article/list", aCon.ArticleListHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
-  r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", aCon.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", aCon.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", aCon.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
 
-  return r
+	r.Use(middlewares.LoggingMiddleware)
+
+	return r
 }
